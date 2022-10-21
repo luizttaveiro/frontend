22148e74e66952e2b6a4a15f0e51991a329ab306
Gustavo <gustavo987987@gmail.com>
2022-10-20 22:34:21 -0300

    data biding e pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,11 @@ import { HomeComponent } from './components/home/home.component';
 import { ListarprodutosComponent } from './components/produtos/listarprodutos/listarprodutos.component';
 import { CadastrarprodutoComponent } from './components/produtos/cadastrarproduto/cadastrarproduto.component';
 
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localePt);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,13 +21,13 @@ import { CadastrarprodutoComponent } from './components/produtos/cadastrarprodut
     FooterComponent,
     HomeComponent,
     ListarprodutosComponent,
-    CadastrarprodutoComponent
+    CadastrarprodutoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [BrowserModule, AppRoutingModule],
+
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,127 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public nomeProduto: string = 'Curso de Angular';
+  anuncio: string = `O ${this.nomeProduto} está em promoção`;
+  idProduto: number = 3;
+  precoProduto: number = 2.51;
+  promocao: boolean = true;
+  foto: string = 'assets/img/crud.png'
+  dataValidade = '2021-12-31'
 
-  constructor() { }
+  constructor() {
+    //this.anuncio = 'O ' + this.nomeProduto + ' está em promoção';
 
-  ngOnInit(): void {
+    console.log('Nome do Produto: ', this.nomeProduto);
+    console.log('Anuncio: ', this.anuncio);
+    console.log('Id: ', this.idProduto);
+    console.log('Preço: ', this.precoProduto);
+    console.log('Promoção: ', this.promocao);
+
+    /*
+    var variavel1;
+    let variavel2;
+    const variavel : number = 10;*/
+    /*var idade = 15;
+    console.log('Minha idade é: ', idade);
+    let idadeLet = 20;
+    console.log('Minha idade é: ', idadeLet);*/
+    /* function imprimeIdade{
+      var idade = 20;
+      console.log('Minha idade', idade);
+
+    }*/
+    /*function imprimeIdade(){
+      let idade = 20;
+      console.log('Sua idade é: ', idade);
+
+    }
+    imprimeIdade();
+
+    function imprimeIdadeFor(){
+      for(var idade = 30; idade <= 40; idade++){
+        console.log('Idade dentro do for: ', idade);
+      }
+      console.log('Idade fora do for: ', idade);
+    }
+*/
+    /*  let idade =  15;
+    function imprimeIdadeFor(){
+      for(let idade = 30; idade <= 40; idade ++){
+        console.log('Idade dentro do for: ', idade);
+      }
+      console.log('Idade fora do for: ', idade);
+    }
+    imprimeIdadeFor();
+
+    let variavel = 0;
+    let variavel = 10;
+
+    console.log ('variavel', variavel);
+    function funcao(){
+    let outra_variavel = 20;
+    let  outra_variavel = 30;
+    console.log('outra variavel:',outra_variavel);
+
+    }
+    funcao();
+*/
+    /*
+var a = 10;
+    let b = 20;
+
+    function bar() {
+      var a = 30;
+      let b = 40;
+      if(true){
+        var a = 50;
+        let b = 60;
+        console.log('Variável a dentro do if', a);
+        console.log('Variável b dentro do if', b);
+
+      }
+      console.log('Variável a dentro da função', a);
+      console.log('Variável b dentro da função', b);
+    }
+    bar();
+    console.log('Variável a fora da função', a);
+    console.log('Variável b fora da função', b);
+*/
+    /*let a = 1;
+function bar() {
+  console.log('variável a: ', a);
+  let b = 2;
+  if(true){
+    let c = 3;
+    let a = 4;
+    console.log('variável a dentro do if: ', a)
+    console.log('variável b dentro do if: ', b);
+    console.log('variável c dentro do if: ', c);
+  }
+  console.log('variável a fora do if: ', a)
+  console.log('variável b fora do if: ', b)
+}
+   bar();
+   */
+    /*
+   var a = 5;
+   var b = 10;
+    if(a = 5){
+      let a = 4;
+      var b = 1;
+      console.log('Variável a dentro do if: ', a);
+      console.log('Variável b dentro do if: ', b);
+
+
+    }
+    console.log('Variável a fora do if: ', a);
+    console.log('Variável b fora do if: ', b);
+
+    */
   }
 
+  ngOnInit(): void {}
 }
